8c2f89627faab1b37f53c9d85d96290a740fba57
chubbytoey <toey.sasikrang@gmail.com>
2020-11-15 22:28:48 +0700

    add BannerList Component

---
+++ components/BannerList.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="banner-list">
+    <div class="banner-list-main">
+      {{ number }}
+    </div>
+    <div class="banner-list-sub">
+      {{ description }}
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  props: {
+    number: {
+      type: String,
+      default: '0'
+    },
+    description: {
+      type: String,
+      default: ' '
+    }
+  }
+}
+</script>
+<style lang="scss" scoped>
+.banner-list {
+    display: flex;
+    height: 83px;
+    width: 230px;
+    & > .banner-list-main {
+        font-size: 48px;
+        font-weight: 400;
+        border-bottom: 1px solid #fff;
+        width: 114px;
+        display: flex;
+        justify-content: center;
+    }
+    & > .banner-list-sub {
+        font-size:14px ;
+        font-weight: 400;
+        padding-left:24px;
+        padding-top:13px;
+    }
+}
+</style>

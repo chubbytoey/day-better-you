9be6aab092e93ba44dfce18c00addf08115cda25
chubbytoey <toey.sasikrang@gmail.com>
2020-11-19 17:57:53 +0700

    edit default layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,18 +1,18 @@
 <template>
-  <div>
-    <Topbar />
-    <Nuxt />
-    <Footer />
-  </div>
+    <div>
+        <Topbar />
+        <Nuxt />
+        <Footer />
+    </div>
 </template>
 
 <script>
 import Topbar from '../components/Topbar.vue'
 import Footer from '../components/Footer.vue'
 export default {
-  components: {
-    Topbar, Footer
-  }
+    components: {
+        Topbar, Footer
+    }
 }
 </script>
 

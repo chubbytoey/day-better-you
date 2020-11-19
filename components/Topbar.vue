f45ecbd23cfd3ea815574b0304d24a3d3a42473d
chubbytoey <toey.sasikrang@gmail.com>
2020-11-19 17:57:42 +0700

    edit Topbar component

--- components/Topbar.vue
+++ components/Topbar.vue
@@ -1,22 +1,31 @@
 <template>
-  <div class="topbar-container">
-    <div class="logo-container">
-      <div class="logo-icon">
-        <img src='../assets/logo.svg'>
-      </div>
-      <label>day better you</label>
+    <div class="topbar-container">
+        <div class="block">
+            <div class="logo-container">
+                <div class="logo-icon">
+                    <img src="../assets/images/logo.svg">
+                </div>
+                <label>day better you</label>
+            </div>
+            <div class="menu-container">
+                <div class="menu">
+                    <nuxt-link to="/" class="list active">
+                        หน้าแรก
+                    </nuxt-link>
+                    <nuxt-link to="/" class="list">
+                        โปรแกรม
+                    </nuxt-link>
+                    <nuxt-link to="/" class="list">
+                        ติดต่อเรา
+                    </nuxt-link>
+                </div>
+                <a-divider class="line-divider" type="vertical" />
+                <a-button class="custom-button" type="primary">
+                    เริ่มโปรแกรม
+                </a-button>
+            </div>
+        </div>
     </div>
-
-    <div class="menu-container">
-      <div class="menu">
-        <div class="list"><b>หน้าแรก</b></div>
-        <div class="list">โปรแกรม</div>
-        <div class="list">ติดต่อเรา</div>
-      </div>
-      <a-divider class="line-divider" type="vertical" />
-      <a-button class="custom-button" type="primary">เริ่มโปรแกรม</a-button>
-    </div>
-  </div>
 </template>
 
 <script>
@@ -28,64 +37,67 @@ export default {
 <style lang="scss" scoped>
 
 .topbar-container {
-  border-top: $based-color 4px solid;
   width: 100%;
-  color:#fff;
   background-color: #fff;
-  height: 64px;
-  display: flex;
-  font-size: 14px;
-  justify-content: space-between;
-  color: $based-color;
-  padding:0 55px;
-
-/* &.red {
-    background-color:red;
-} */
-    & > .logo-container {
-        text-transform: uppercase;
+  border-top: $based-color 4px solid;
+    & > .block {
+        max-width: 1280px;
+        min-height: 64px;
+        margin: 0 auto;
+        padding: 0 56px;
         display: flex;
-        width: 210px;
-        font-weight: 700;
-        justify-content: center;
+        flex-wrap: wrap;
         align-items: center;
-        font-size: 20px;
-
-        & > .logo-icon {
-            width: 32px;
-            height: 32px;
-
-            & > img {
-              width: 100%;
-            }
-        }
-
-        & > label {
-            padding-left:10px;
-        }
-    }
-
-    & > .menu-container {
-        display: flex;
-        align-items: center;
-
-        & > .menu {
-            padding:10px;
+        justify-content: space-between;
+        & > .logo-container {
+            text-transform: uppercase;
             display: flex;
-            color:#000;
-            height: 64px;
+            justify-content: center;
             align-items: center;
-
-            & > .list {
-                padding: 0 19px;
+            & > .logo-icon {
+                width: 32px;
+                height: 32px;
+                margin-right:10px;
+                & > img {
+                    width: 100%;
+                    height: 100%;
+                }
+            }
+            & > label {
+                font-size: 1.25rem;
+                font-weight: 700;
+                color: $based-color;
             }
         }
 
-        & > .line-divider {
-            height: 32px;
-            margin-right: 24px;
-        }
+        & > .menu-container {
+            display: flex;
+            font-size: 0.875rem;
+            align-items: center;
+            & > .menu {
+                padding:10px;
+                display: flex;
+                color:#000;
+                height: 64px;
+                align-items: center;
 
+                & > .list {
+                    padding: 0 19px;
+                }
+                & > a {
+                    color:$based-color;
+                }
+                & > .active {
+                    font-weight: bold;
+                }
+            }
+
+            & > .line-divider {
+                height: 32px;
+                margin-right: 24px;
+            }
+
+        }
     }
 }
 </style>
